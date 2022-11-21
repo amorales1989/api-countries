d2ca051192bda1c1f899aad91333d55ac51cf2ec
Alejandro Morales <68878611+amorales1989@users.noreply.github.com>
2022-11-21 02:12:39 -0300

    Update index.js

--- PI-Countries-main/api/index.js
+++ PI-Countries-main/api/index.js
@@ -24,7 +24,7 @@ require('dotenv').config();
 const {PORT} = process.env
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(`${PORT}`, () => {
+  server.listen(PORT, () => {
     getApiInfo();
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
