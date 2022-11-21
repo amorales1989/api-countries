b097ad029fdde6a9131a3bdfcd2b8cc955c0444f
alejandro <valentina.morales.paratore@gmail.com>
2022-11-20 23:57:23 -0300

    deploy DB

--- PI-Countries-main/api/src/db.js
+++ PI-Countries-main/api/src/db.js
@@ -3,10 +3,12 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  // DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, 
+  PGUSER, PGPASSWORD, PGHOST, PGPORT, PGDATABASE,
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, {
+// const sequelize = new Sequelize(`postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`
+const sequelize = new Sequelize(`postgresql://${{ PGUSER }}:${{ PGPASSWORD }}@${{ PGHOST }}:${{ PGPORT }}/${{ PGDATABASE }}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
